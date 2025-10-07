e020e5088605c3fce76229bf4d889587170f0499
PavelChukharev <pchukharev@kapitus.com>
2025-10-07 11:48:07 +0400

    add home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.components'),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+  },
+];
